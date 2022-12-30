881660765431d9146c6cc87a56371b83683e6b22
Sergvah <104735864+Sergvah@users.noreply.github.com>
2022-12-30 13:05:11 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/test-marshroutes-po-video-repety">
       <App />
     </BrowserRouter>
   </React.StrictMode>
